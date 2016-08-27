f0291a33b88aa4697666d8204af5727ed5c66d58
Gabriel C. Troia <catalin.troia@gmail.com>
2016-08-27 18:49:30 -0400

    Restructure and refactor

---
+++ src/MTA/index.js
@@ -0,0 +1,29 @@
+'us strict';
+
+const util = require('./util');
+const dataUtil = require('./dataUtil');
+
+const getData = () => {
+  return util.getDataFromFile('./data/output_sample.xml');
+
+  // return util.getDataFromURL('http://web.mta.info/status/serviceStatus.txt');
+}
+
+const getAllLines = () => getData()
+    .then(util.parseXML)
+    .then(r => r.service)
+    .then(dataUtil.getAllSubwayLines);
+
+
+const getStatusForStation = station => Promise
+    .resolve(getAllLines())
+    .then(dataUtil.lines.findByStation(station));
+
+const getStatusForTrain = train => Promise
+  .resolve(getAllLines())
+  .then(dataUtil.lines.findByTrain(train));
+
+module.exports = {
+  getStatusForStation,
+  getStatusForTrain,
+}
--- src/index.js
+++ src/index.js
@@ -1,35 +1,19 @@
 'us strict';
 
-const R = require('ramda');
+require('dotenv').load();
 
-const util = require('./util');
-const dataUtil = require('./dataUtil');
 const AlexaSkill = require('./AlexaSkill');
+const mta = require('./MTA');
 
-const ALEXA_APP_ID = 'amzn1.ask.skill.5dd35a53-9e0b-4186-ba24-32daf27b857f';
-const MTA_KEY = 'a4a2ec544d16a58b4e277bee25f23562';
+const ALEXA_APP_ID = process.ALEXA_APP_ID;
 
-const getData = () => {
-  // return util.getDataFromFile('../output.xml');
-
-  return util.getDataFromURL('http://web.mta.info/status/serviceStatus.txt');
-}
-
-
-
-getData()
-  .then(util.parseXML)
-  .then(r => r.service)
-  .then(dataUtil.getAllSubwayLines)
-  // .then(dataUtil.lines.findByStation('metropolitan'))
-  // .then(R.filter(dataUtil.lines.onTime))
+Promise
+  .resolve(mta.getStatusForStation('grands'))
   .then(lines => {
-    // console.log('lines', lines)
-
     lines.forEach(l => {
       console.log('');
       console.log('------ Line:', l.name[0], '-----');
-      console.log('date', l.date);
+      console.log('date', l.Date[0], l.Time[0]);
       console.log('status', l.status);
       console.log('info', l.text);
       console.log('');
@@ -38,5 +22,6 @@ getData()
 
     console.log('summary:');
 
-    console.log('lines #', lines.length)
+    console.log('lines #', lines.length);
   });
+
